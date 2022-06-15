8328e49fc2c43755d42d5708fac3f5e9ce7c33e2
Emre Fatih Gökçe <emrefatih47@hotmail.com>
2022-06-15 23:07:53 +0300

    Add project files.

---
+++ AbstractFactory/Class1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    //public class AFP //abstract factory project
+    //{
+
+    //}
+
+    public interface IAbstractFactory
+    {
+        IAbstractProductA CreateProductA();
+
+        IAbstractProductB CreateProductB();
+    }
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+    }
+
+    public interface IAbstractProductB
+    {
+        // Product B is able to do its own thing...
+        string UsefulFunctionB();
+
+        // ...but it also can collaborate with the ProductA.
+        //
+        // The Abstract Factory makes sure that all products it creates are of
+        // the same variant and thus, compatible.
+        string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    }
+
+    public class ConcreteFactory1 : IAbstractFactory
+    {   
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA1();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB1();
+        }
+    }
+
+    public class ConcreteProductA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A1.";
+        }
+    }
+
+    public class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A2.";
+        }
+    }
+
+    // Concrete Products are created by corresponding Concrete Factories.
+    public class ConcreteProductB1 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B1.";
+        }
+
+        // The variant, Product B1, is only able to work correctly with the
+        // variant, Product A1. Nevertheless, it accepts any instance of
+        // AbstractProductA as an argument.
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B1 collaborating with the ({result})";
+        }
+    }
+
+    public class ConcreteProductB2 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B2.";
+        }
+
+        // The variant, Product B2, is only able to work correctly with the
+        // variant, Product A2. Nevertheless, it accepts any instance of
+        // AbstractProductA as an argument.
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B2 collaborating with the ({result})";
+        }
+    }
+
+    public class ConcreteFactory2 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA2();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB2();
+        }
+    }
+
+    public class Client
+    {
+        public void Main()
+        {
+            // The client code can work with any concrete factory class.
+            Console.WriteLine("Client: Testing client code with the first factory type...");
+            ClientMethod(new ConcreteFactory1());
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Testing the same client code with the second factory type...");
+            ClientMethod(new ConcreteFactory2());
+        }
+
+        public void ClientMethod(IAbstractFactory factory)
+        {
+            var productA = factory.CreateProductA();
+            var productB = factory.CreateProductB();
+
+            Console.WriteLine(productB.UsefulFunctionB());
+            Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+        }
+    }
+}
